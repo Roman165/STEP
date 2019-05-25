ab8f65f9de74e9b153bdde6780d4dfcad6b61ce3
Roman165 <roman16545@gmail.com>
2019-05-25 18:46:48 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,48 @@
+(function(){
+$('.slider-2').slick({
+  arrows: false,
+  centerMode: true,
+  centerPadding: '-20px',
+  slidesToShow: 3,
+  dots: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$('#slider').on('init reInit',function(event,slick){
+  var amount = slick.slideCount;
+  $('#range').attr('max',amount);
+})
+
+$('#slider').on('afterChange',function(e,slick,currentSlide){
+  $('#range').val(currentSlide+1);
+})
+
+$('#range').on('input change',function(){
+  $('#slider').slick('slickGoTo',this.value-1);
+});
+
+$('#slider').slick({
+  centerMode: true,
+  slidesToShow:3,
+  arrows:false,
+  dots:false,
+  centerPadding: '-20px',
+})
+})()
